4535a38215ec2c301735e098fae0640fa4799f96
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-05-27 17:12:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,16 @@
 # Write your code here.
-def line(deli)
-  if deli.size == 0
+def line(katz_deli)
+  if katz_deli.size == 0
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
-    deli.each_with_index do |name, i|
+    katz_deli.each_with_index do |name, i|
       current_line += " #{i+1}. #{name}"
     end
     puts(current_line)
   end
 end
 
-def take_a_number
+def take_a_number(deli, name)
+  
 end
