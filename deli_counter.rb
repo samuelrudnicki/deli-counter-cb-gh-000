6760b4516f0a9ee60ef037f72e24bb4aee420fac
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-05-27 17:06:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,5 +2,10 @@
 def line(deli)
   if deli.size == 0
     puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    deli.each_with_index do |i, name|
+      current_line += " #{i}. #{name}"
   end
+  
 end
