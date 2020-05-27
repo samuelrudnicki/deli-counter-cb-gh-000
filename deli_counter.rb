37c9e146435f1cad2756ccf402920f42698bd78c
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-05-27 17:14:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,6 +11,7 @@ def line(katz_deli)
   end
 end
 
-def take_a_number(deli, name)
-
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
